f763321bd3636f97d36be979b7251edfeee62f6d
Sergio Juarez <bleding.death@gmail.com>
2020-04-26 20:29:50 -0500

    Deletes unused exception

--- src/main/java/mx/kinich49/itemtracker/exceptions/ShoppingNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package mx.kinich49.itemtracker.exceptions;
-
-public class ShoppingNotFoundException extends Exception {
-
-    public ShoppingNotFoundException(long id){
-        super("Shopping with id " + id + " not found");
-    }
-}
