455e0a2adb2901a3fb0ded57e48fb04362480355
Sergio Juarez <bleeding.death@gmail.com>
2020-05-23 13:47:15 -0500

    Adds CrossOrigin to SuggestionsController

--- src/main/java/mx/kinich49/itemtracker/controllers/SuggestionController.java
+++ src/main/java/mx/kinich49/itemtracker/controllers/SuggestionController.java
@@ -5,13 +5,12 @@ import mx.kinich49.itemtracker.services.SuggestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/suggestions")
+@SuppressWarnings("unused")
+@CrossOrigin
 public class SuggestionController {
 
     private final SuggestionService suggestionService;
