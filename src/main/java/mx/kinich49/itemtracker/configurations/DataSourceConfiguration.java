af2f3b7e5e735fc5f8e00ff9d1b2819589cadbc2
Sergio Juarez <bleeding.death@gmail.com>
2020-06-22 22:42:45 -0500

    Adds production profile, uses MariaDB as RDBMS for production

---
+++ src/main/java/mx/kinich49/itemtracker/configurations/DataSourceConfiguration.java
@@ -0,0 +1,31 @@
+package mx.kinich49.itemtracker.configurations;
+
+import org.mariadb.jdbc.MariaDbDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@Configuration()
+@Profile(value = {"production"})
+public class DataSourceConfiguration {
+
+    Environment environment;
+
+    @Autowired
+    public DataSourceConfiguration(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "app.datasource")
+    public DataSource dataSource() throws SQLException {
+        //Password must be injected via command-line argument
+        return new MariaDbDataSource();
+    }
+}
