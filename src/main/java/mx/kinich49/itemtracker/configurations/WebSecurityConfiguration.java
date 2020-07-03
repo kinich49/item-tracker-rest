31b20357e5a2af87ac9b318c90f202dfcb881750
Sergio Juarez <bleeding.death@gmail.com>
2020-07-03 18:19:42 -0500

    Fixes bug with security configuration. CSRF is disabled and Basic Auth with hardcoded password is enabled

--- src/main/java/mx/kinich49/itemtracker/configurations/WebSecurityConfiguration.java
+++ src/main/java/mx/kinich49/itemtracker/configurations/WebSecurityConfiguration.java
@@ -20,6 +20,10 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and().httpBasic()
+                .and()
+                .csrf().disable();
     }
 }
