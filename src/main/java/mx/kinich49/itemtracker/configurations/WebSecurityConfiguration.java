30ab5b8a256d362b694183158c328adfbb1e8a93
Sergio Juarez <bleeding.death@gmail.com>
2020-07-03 13:11:55 -0500

    Disables CSRF

--- src/main/java/mx/kinich49/itemtracker/configurations/WebSecurityConfiguration.java
+++ src/main/java/mx/kinich49/itemtracker/configurations/WebSecurityConfiguration.java
@@ -2,16 +2,24 @@ package mx.kinich49.itemtracker.configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
+@SuppressWarnings("unused")
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**")
+                .antMatchers("/");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
     }
 }
